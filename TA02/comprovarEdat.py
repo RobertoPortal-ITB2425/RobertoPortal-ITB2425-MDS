8577690dc09684569ef2d490c7176a7ce4b6f8b1
roberto.portal.7e8 <roberto.portal.7e8@itb.cat>
2024-10-01 12:42:21 +0200

    # This is a comment
    print(Comprovar edat)

---
+++ TA02/comprovarEdat.py
@@ -0,0 +1,12 @@
+"""
+Roberto Portal
+01/10/2024
+ASIXc1C MDS TA02
+Descripció: Exemple dels apunts
+"""
+# Programa que demana l'edat i diu si ets major d'edat.
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+
+print("Programa Finalitzat")
