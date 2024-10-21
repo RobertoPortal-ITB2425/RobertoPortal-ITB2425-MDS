25e33ad201cc2cab7758c889e7792e75803c8122
roberto.portal.7e8 <roberto.portal.7e8@itb.cat>
2024-10-21 08:51:43 +0200

    # Ni idea de que es esto

--- TA03/Incidencias2.py
+++ TA03/Incidencias2.py
@@ -1,12 +1,16 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse("country_data.xml")
+
+from PIL.ImageOps import contain
+
+tree = ET.parse("TA03.xml")
 root = tree.getroot()
 
 root.tag
-"data"
+"row"
 root.attrib
 {}
-
+cont = 0
 for child in root:
     print(child.tag, child.attrib)
-
+    cont =cont +1
+print(cont)
